6b383107693d2bb3e5d98350ee8d64f9510bd353
swilson <swilson@kcg.com>
2014-08-09 09:37:37 +0100

    Made the read interface work (respond to ready correctly)

--- ddr3_control/read_from_ddr3.v
+++ ddr3_control/read_from_ddr3.v
@@ -31,10 +31,11 @@ module read_from_ddr3 #(parameter IMAGE_WIDTH  = 1280,
 
 );
 
-parameter IDLE             = 2'd0;
-parameter START_READ       = 2'd1;
-parameter START_TEST_READ  = 2'd2;
-parameter WAIT_FOR_DATA    = 2'd3;
+parameter IDLE                   = 3'd0;
+parameter START_READ             = 3'd1;
+parameter START_TEST_READ        = 3'd2;
+parameter WAIT_FOR_DATA          = 3'd3;
+parameter WAIT_FOR_FIFO_TO_EMPTY = 3'd4;
 
 parameter MAX_COUNT = (((IMAGE_WIDTH*IMAGE_HEIGHT)>>2)-1);
 
@@ -52,106 +53,99 @@ reg         next_buffer_sel, buffer_sel;
 reg         next_rd_finish, rd_finish;
 reg [127:0] next_test_rd_data;
 
+
 always @(*)
 begin
-  next_state               = state;
-  next_ddr3_avl_burstbegin = 1'b0;
-  next_ddr3_avl_read_req   = 1'b0;
-  next_transfer_count      = transfer_count;
-  next_buffer_sel          = buffer_sel;
-  next_clear_buffer0       = 1'b0;
-  next_clear_buffer1       = 1'b0;
-  next_ddr3_avl_addr       = ddr3_avl_addr;
-  next_test_rd_data        = test_rd_data;
-  next_rd_finish           = 1'b0;
-  next_ddr3_avl_size       = ddr3_avl_size;
+    next_state               = state;
+    ddr3_avl_burstbegin      = 1'b0;
+    ddr3_avl_read_req        = 1'b0;
+    next_transfer_count      = transfer_count;
+    next_buffer_sel          = buffer_sel;
+    next_clear_buffer0       = 1'b0;
+    next_clear_buffer1       = 1'b0;
+    next_ddr3_avl_addr       = ddr3_avl_addr;
+    next_test_rd_data        = test_rd_data;
+    next_rd_finish           = 1'b0;
+    next_ddr3_avl_size       = ddr3_avl_size;
 
-  case (state)
-    IDLE:
+    case (state)
+        IDLE:
+            if (test_rd)
+            begin
+                next_state          = START_TEST_READ;
+                next_ddr3_avl_addr  = test_addr[25:0];
+                next_ddr3_avl_size  = 3'b100;
+            end
+            else if (!ddr3_rd_buffer0_empty || !ddr3_rd_buffer1_empty)
+            begin
+                if (!ddr3_rd_buffer0_empty && (buffer_sel == 1'b0))
+                    next_ddr3_avl_addr       = ddr3_buffer0_offset;
+                else if (!ddr3_rd_buffer1_empty && (buffer_sel == 1'b1))
+                    next_ddr3_avl_addr       = ddr3_buffer1_offset;
+                next_state          = START_READ;
+                next_ddr3_avl_size  = 3'b001;
+            end
 
-      if (test_rd)
-      begin
-          next_ddr3_avl_burstbegin = 1'b1;
-          next_ddr3_avl_read_req   = 1'b1;
-	  next_ddr3_avl_addr       = test_addr[25:0];
-          next_state               = START_TEST_READ;
-          next_ddr3_avl_size       = 3'b001;
-      end
-      else if (!ddr3_rd_buffer0_empty || !ddr3_rd_buffer1_empty)
-      begin
-        if (!ddr3_rd_buffer0_empty && (buffer_sel == 1'b0))
-          next_ddr3_avl_addr       = ddr3_buffer0_offset;
-        else if (!ddr3_rd_buffer1_empty && (buffer_sel == 1'b1))
-          next_ddr3_avl_addr       = ddr3_buffer1_offset;
-        next_state = START_READ;
-        next_ddr3_avl_burstbegin = 1'b1;
-        next_ddr3_avl_read_req   = 1'b1;
-        next_ddr3_avl_size       = 3'b100;
-        next_transfer_count      = 'd0;
-      end
-
-    START_TEST_READ:
-      if (!ddr3_avl_ready)
-      begin
-        next_ddr3_avl_burstbegin = 1'b1;
-        next_ddr3_avl_read_req   = 1'b1;
-      end
-      else if (ddr3_avl_ready)
-      begin
-        next_state = WAIT_FOR_DATA;
-      end
-
-    WAIT_FOR_DATA:
-      if (ddr3_avl_read_data_valid)
-      begin
-        next_test_rd_data   = ddr3_avl_read_data;
-        next_rd_finish      = 1'b1;
-        next_state          = IDLE;
-      end
- 
-    START_READ:
-      if (!ddr3_avl_ready && ~data_fifo_almost_full)
-      begin
-        next_ddr3_avl_burstbegin = 1'b1;
-        next_ddr3_avl_read_req   = 1'b1;
-      end
-      else if (ddr3_avl_ready)
-      begin
-        if (transfer_count == MAX_COUNT)
+        START_TEST_READ:
         begin
-          if (~buffer_sel)
-          begin
-            if (!ddr3_rd_buffer1_empty) begin
-              next_buffer_sel = 1'b1;
-              next_clear_buffer0 = 1'b1;
-            end
-            else begin
-              next_buffer_sel = 1'b0;
-            end
-          end
-          else 
-          begin
-            if (!ddr3_rd_buffer0_empty) begin
-              next_buffer_sel = 1'b0;
-              next_clear_buffer1 = 1'b1;
-            end
-            else begin
-              next_buffer_sel = 1'b1;      
-            end
-          end
-          next_state      = IDLE;
-        end 
-        else if (~data_fifo_almost_full) 
-        begin
-          next_ddr3_avl_addr       = ddr3_avl_addr + 'd4;
-          next_ddr3_avl_burstbegin = 1'b1;
-          next_ddr3_avl_read_req   = 1'b1;
-          next_transfer_count      = transfer_count + 'd1;
+            ddr3_avl_burstbegin = 1'b1;
+            ddr3_avl_read_req   = 1'b1;
+
+            if (!ddr3_avl_ready)
+                next_state          = WAIT_FOR_DATA;
         end
-      end
-  endcase
-end
 
+        WAIT_FOR_DATA:
+            if (ddr3_avl_read_data_valid)
+            begin
+                next_test_rd_data   = ddr3_avl_read_data;
+                next_rd_finish      = 1'b1;
+                next_state          = IDLE;
+            end
+
+        START_READ:
+        begin
+            ddr3_avl_burstbegin = 1'b1;
+            ddr3_avl_read_req   = 1'b1;
+
+            if (!ddr3_avl_ready)
+                if (data_fifo_almost_full)
+                    next_state      = WAIT_FOR_FIFO_TO_EMPTY;
+                else if (transfer_count == MAX_COUNT)
+                begin
+                  if (~buffer_sel)
+                  begin
+                    if (!ddr3_rd_buffer1_empty) begin
+                      next_buffer_sel = 1'b1;
+                      next_clear_buffer0 = 1'b1;
+                    end
+                    else begin
+                      next_buffer_sel = 1'b0;
+                    end
+                  end
+                  else 
+                  begin
+                    if (!ddr3_rd_buffer0_empty) begin
+                      next_buffer_sel = 1'b0;
+                      next_clear_buffer1 = 1'b1;
+                    end
+                    else begin
+                      next_buffer_sel = 1'b1;      
+                    end
+                  end
+                  next_state      = IDLE;
+                end 
+                else
+                begin
+                    next_ddr3_avl_addr       = ddr3_avl_addr + 'd4;
+                    next_transfer_count      = transfer_count + 'd1;
+                end
+        end
+        WAIT_FOR_FIFO_TO_EMPTY:
+            next_state = START_READ;
+    endcase
+end
+ 
 async_handshake i_async_handshake_clear0 (
 	.req_clk     (ddr3_clk),
 	.ack_clk     (clk),
@@ -181,8 +175,6 @@ always @(posedge ddr3_clk or negedge ddr3_reset_n)
   begin
     state                 <= IDLE;
     ddr3_avl_addr         <= 26'd0;
-    ddr3_avl_burstbegin   <= 1'b0;
-    ddr3_avl_read_req     <= 1'b0;
     ddr3_avl_size         <= 3'd0;
     transfer_count        <= 'd0;
     buffer_sel            <= 1'b0;
@@ -195,8 +187,6 @@ always @(posedge ddr3_clk or negedge ddr3_reset_n)
   begin
     state                 <= next_state;
     ddr3_avl_addr         <= next_ddr3_avl_addr;
-    ddr3_avl_burstbegin   <= next_ddr3_avl_burstbegin;
-    ddr3_avl_read_req     <= next_ddr3_avl_read_req;
     ddr3_avl_size         <= next_ddr3_avl_size;
     transfer_count        <= next_transfer_count;
     buffer_sel            <= next_buffer_sel;
